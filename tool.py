0394cec764fad54765a3b8d9c90e79a154591ad9
Raven <iptime0817@naver.com>
2025-03-26 14:55:43 +0900

    최초 생성

---
+++ tool.py
@@ -0,0 +1,22 @@
+# test
+
+import pandas as pd
+import numpy as np
+import random
+
+#--------------------------------------------------
+
+# 무작위 난수
+a=random.randint(1,6)
+print(a)
+
+# 참고용
+'''
+1 : 소방청 구급 활동정보 #csv
+2 : 기상청 생활기상지수 #xlsx <<< 제외
+3 : 관광 소비행태 데이터(제주) #xls,csv
+4 : 전국 온도(혹은 서울 온도) #csv
+5 : 산림청 산불발생통계 #openAPI
+6 : 전국 야영장 등록 현황 #csv
+'''
+print('='*80)
