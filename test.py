5fdbff860dd52edf153ec8c4f15bb5bf01e814d3
Raven <iptime0817@naver.com>
2025-03-27 11:38:24 +0900

    03-27 오전 작업

--- test.py
+++ test.py
@@ -51,70 +51,50 @@ def checker(data):
   except:
     print('>>> 경고! 데이터 형식이 잘못되었습니다!\n>>> checker(data) / repeat= 샘플 출력 횟수')
 
-path2020 = 'C:/Mtest/project_first/pjdata/소방청_전국 산악사고 구조활동현황_20201231.csv'
-path2023 = 'C:/Mtest/project_first/pjdata/소방청_전국 산악사고 현황_20231231.csv'
+pathfind = 'C:/Mtest/project_first/pjdata/seoul/서울.csv'
+path2 = 'C:/Mtest/project_first/pjdata/jeju-island/성산2.csv'
 
 #--------------------------------------------------
 
-df2020 = pd.read_csv(path2020,encoding='euc-kr')
-df2023 = pd.read_csv(path2023,encoding='euc-kr')
-checker(df2023)
-'''
- #   Column        Non-Null Count  Dtype 
----  ------        --------------  ----- 
- 0   신고년월일         13189 non-null  object
- 1   신고시각          13189 non-null  object
- 2   출동년월일         13029 non-null  object
- 3   출동시각          13029 non-null  object
- 4   발생장소_시        13189 non-null  object
- 5   발생장소_구        13189 non-null  object
- 6   발생장소_동        13189 non-null  object
- 7   발생장소_리        13189 non-null  object
- 8   번지            13090 non-null  object
- 9   사고원인          13189 non-null  object
- 10  사고원인코드명_사고종별  13189 non-null  object
- 11  구조인원          13189 non-null  int64
- 행: 13189, 열: 12
+# 컬럼명을 수동으로 지정해 1~7행 무시
+columns = ["년월", "지점", "평균기온(℃)", "평균최저기온(℃)", "평균최고기온(℃)"]
+data = pd.read_csv(pathfind, encoding="cp949", skiprows=8, names=columns)
 
-  #   Column        Non-Null Count  Dtype 
----  ------        --------------  ----- 
- 0   신고년월일         21189 non-null  object
- 1   신고시각          21189 non-null  object
- 2   발생장소_시        21189 non-null  object
- 3   발생장소_구        21189 non-null  object
- 4   발생장소_동        21123 non-null  object
- 5   발생장소_리        21123 non-null  object
- 6   사고원인          21189 non-null  object
- 7   사고원인코드명_사고종별  21189 non-null  object
- 8   처리결과코드        21189 non-null  object
- 9   구조인원          21189 non-null  int64
- 행: 21189, 열: 10
-'''
-# call(df.head(10),'plane')
-'''
-    신고년월일    신고시각    출동년월일    출동시각    발생장소_시    발생장소_구    발생장소_동    발생장소_리    번지    사고원인    사고원인코드명_사고종별      구조인원
---  ------------  ----------  ------------  ----------  -------------  -------------  -------------  -------------  ------  ----------  -------------------------  ----------
- 0  2020-02-01    15:25       2020-02-01    15:29       서울특별시     서대문구       홍제동         홍제동         산1-1   실족추락    실족추락                            1
- 1  2020-02-01    14:34       2020-02-01    14:40       서울특별시     종로구         옥인동         옥인동         산3-14  실족추락    실족추락                            1
- 2  2020-04-25    10:53       2020-04-25    10:57       서울특별시     종로구         누상동         누상동         산1-27  실족추락    실족추락                            1
- 3  2020-07-12    10:55       2020-07-12    10:58       서울특별시     종로구         청운동         청운동         산1-1   개인질환    개인질환                            0
- 4  2020-02-19    06:00       2020-02-19    06:06       서울특별시     종로구         청운동         청운동         산4-36  실족추락    실족추락                            0
- 5  2020-05-31    12:25       2020-05-31    12:32       서울특별시     종로구         신영동         신영동         82      기타산악    기타산악                            0
- 6  2020-04-25    09:35       2020-04-25    09:47       서울특별시     성북구         정릉동         정릉동         산1-1   기타산악    기타산악                            1
- 7  2020-11-03    14:02       2020-11-03    14:05       서울특별시     종로구         평창동         평창동         57-1    일반조난    일반조난                            0
- 8  2020-06-09    12:55       nan           nan         서울특별시     종로구         구기동         구기동         산3-1   기타산악    기타산악                            0
- 9  2020-05-10    14:52       2020-05-10    16:36       서울특별시     종로구         구기동         구기동         산3-1   기타산악    기타산악                            1
-'''
-df2020.drop('사고원인코드명_사고종별',axis=1,inplace=True)
-'''
-사고원인 : 산악 1개
-사고원인코드명 : 이것저것
-처리결과코드 : 233종류
+# strip으로 공백제거
+data["년월"] = data["년월"].str.strip()
+
+# 25년 데이터 제외
+testdata = data.tail(3) # 평가할 때 사용
+
+data.drop(data.tail(3).index,inplace=True) # 방안 1
+
+#data = data[~data["년월"].str.contains("2025", na=False)] # 방안 2
+
+# data["년월"] = pd.to_datetime(data["년월"], format="%Y-%m") # 방안 3
+# data["연도"] = data["년월"].dt.year # 연도만 추출
+# data = data[data["연도"] != 2025]  # 2025년 데이터 제거
+call(data.tail())
 
 '''
+ #   Column     Non-Null Count  Dtype  
+---  ------     --------------  -----  
+ 0   년월         639 non-null    object 
+ 1   지점         639 non-null    int64  
+ 2   평균기온(℃)    638 non-null    float64
+ 3   평균최저기온(℃)  638 non-null    float64
+ 4   평균최고기온(℃)  638 non-null    float64
 
-print(df2023.신고년월일.unique())
-# call(df2023[df2023.구조인원>=7],'plane')
-# call(df2023.head(10),'plane')
+     년월       지점    평균기온(℃)    평균최저기온(℃)    평균최고기온(℃)
+--  -------  ------  -------------  -----------------  -----------------
+ 0  1972-01     108            0.8               -2.1                4.5
+ 1  1972-02     108           -0.6               -3.5                3
+ 2  1972-03     108            5.3                1.4               10.3
+ 3  1972-04     108           11.5                6.9               16.7
+ 4  1972-05     108           16.4               11.7               21.3
+
+행: 639, 열: 5
+'''
+for col in data.columns:
+    print(f"data[{col}].isnull()", ':', data[col].isnull().sum())
 
 print('='*80)
